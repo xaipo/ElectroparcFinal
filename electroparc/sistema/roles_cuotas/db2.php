cfeecb8067c60ea0022ad4a59860fdfdffbbeec3
xaipo <xaipoj@hotmail.com>
2017-08-23 08:12:07 -0500

    correciones encode

--- electroparc/sistema/roles_cuotas/db2.php
+++ electroparc/sistema/roles_cuotas/db2.php
@@ -1,11 +1,21 @@
-<?php
+<?php 
 
-$DB_HOST = '127.0.0.1';
-$DB_USER = 'root';
+
+
+$DB_HOST = 'www.electroparc.com';
+
+$DB_USER = 'electroparc_xaipo2';
+
 //$DB_PASS = '23142314';
-$DB_PASS = '123456';
-$DB_NAME = 'idewebtest';
+
+$DB_PASS = 'X@ipocoorp14';
+
+$DB_NAME = 'electroparc_ideweb';
+
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+mysqli_set_charset($mysqli ,"utf8");
+
+
 
 
 ?>
